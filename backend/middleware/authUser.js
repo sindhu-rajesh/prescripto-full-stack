199fa775eda8ea2e2c0d2638d5b2973aabb68139
Sindhu-rajesh <you@example.com>
2025-03-31 14:45:13 +0530

    Updated

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -8,6 +8,7 @@ const authUser = async (req, res, next) => {
     }
     try {
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(token_decode)
         req.body.userId = token_decode.id
         next()
     } catch (error) {
